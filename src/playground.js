2fc992e674b2f96bebe60fa3c711c55496d9e949
Najeeb Arif <NajeebArif786@outlook.com>
2021-04-03 03:08:37 +0530

    Initial Commit

---
+++ src/playground.js
@@ -0,0 +1,14 @@
+export default Playground = (props) => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-6">Column Number 1</div>
+                <div className="col-6">
+                    <span>
+                        <i className="fas fa-home" />
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
